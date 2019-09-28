f0b2b67b459f1c1498238cc351f11ee03633c8a8
DavidBanda <davidbandag96@gmail.com>
2019-09-27 20:49:18 -0600

    final

--- EVA1_EXAMEN_ACREDITA_NOACREDITA/app/src/main/java/com/example/eva1_examen_acredita_noacredita/MainActivity.java
+++ EVA1_EXAMEN_ACREDITA_NOACREDITA/app/src/main/java/com/example/eva1_examen_acredita_noacredita/MainActivity.java
@@ -1,15 +1,78 @@
 package com.example.eva1_examen_acredita_noacredita;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView acreditar, calificacion, acreditado;
+    SeekBar skacreditar, skcali;
+    int acr = 70;
+    int cali = 70;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        acreditar = findViewById(R.id.txtViewAcreditar);
+        calificacion = findViewById(R.id.txtViewCali);
+        acreditado = findViewById(R.id.txtViewAcr);
+        skacreditar = findViewById(R.id.seekBarAcreditar);
+        skcali = findViewById(R.id.seekBarCali);
+
+
+        skcali.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                cali = progress * 10;
+                calificacion.setText("Calificacion: " + String.valueOf(cali));
+                if (cali < acr) {
+                    acreditado.setText("REPROBADO!!!");
+                    acreditado.setTextColor(Color.parseColor("#FFE20E27"));
+                } else {
+                    acreditado.setText("ACREDITADO!!!");
+                    acreditado.setTextColor(Color.parseColor("#FF00574B"));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        skacreditar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                acr = progress * 10;
+                acreditar.setText("Puntos para creditar: " + String.valueOf(acr));
+                if (cali < acr) {
+                    acreditado.setText("REPROBADO!!!");
+                    acreditado.setTextColor(Color.parseColor("#FFE20E27"));
+                } else {
+                    acreditado.setText("ACREDITADO!!!");
+                    acreditado.setTextColor(Color.parseColor("#FF00574B"));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
